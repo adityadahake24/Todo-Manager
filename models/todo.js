6492417c037f9bac358ec4b216b7cafa199e495f
adityadahake24 <aditya.dahake.iot@ghrce.raisoni.net>
2022-12-20 23:48:32 +0530

    Id not defined

--- models/todo.js
+++ models/todo.js
@@ -74,7 +74,10 @@ module.exports = (sequelize, DataTypes) => {
       });
     }
 
-    
+    deletetodo() {
+      return this.remove(id);
+    }
+
     markAsCompleted() {
       return this.update({ completed: true});
     }
